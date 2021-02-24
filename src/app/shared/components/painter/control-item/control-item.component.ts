a75430cb95a3b1d8cd2d1a4f74999a4f31b3962f
peter.chang <peter.chang@aplus-digital.com>
2021-02-24 19:07:47 +0800

    tmp

--- src/app/shared/components/painter/control-item/control-item.component.ts
+++ src/app/shared/components/painter/control-item/control-item.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, Input, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-control-item',
   templateUrl: './control-item.component.html',
   styleUrls: ['./control-item.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ControlItemComponent implements OnInit {
+  @Input()
+  @HostBinding('class.circle')
+  circle = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
