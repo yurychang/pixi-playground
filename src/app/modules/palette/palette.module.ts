88c54886459ca32a007cb469dc536c5dfebb8549
peter.chang <peter.chang@aplus-digital.com>
2021-03-02 16:48:09 +0800

    tmp palette picker background

--- src/app/modules/palette/palette.module.ts
+++ src/app/modules/palette/palette.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '@shared/shared.module';
-import { PaletteComponent } from './palette/palette.component';
+import { PaletteComponent } from './palette.component';
 
 @NgModule({
   declarations: [PaletteComponent],
